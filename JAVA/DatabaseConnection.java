163900a6d0a11cfa07b08259b2b9fd008a3ed5e2
Keerthi <katikakeerthi9@gmail.com>
2025-06-13 12:56:31 +0530

    Update DatabaseConnection.java

--- JAVA/DatabaseConnection.java
+++ JAVA/DatabaseConnection.java
@@ -5,7 +5,7 @@ import java.sql.SQLException;
 public class DatabaseConnection {
     private static final String URL = "jdbc:mysql://localhost:3306/shoppingcartdb";
     private static final String USER = "root"; // Replace with your MySQL username
-    private static final String PASSWORD = "1234"; // Replace with your MySQL password
+    private static final String PASSWORD = "root"; // Replace with your MySQL password
 
     public static Connection getConnection() throws SQLException {
         return DriverManager.getConnection(URL, USER, PASSWORD);
